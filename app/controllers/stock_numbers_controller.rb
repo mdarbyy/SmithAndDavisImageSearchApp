efe02fe6f3925427819f5afaa40517ed23ff019e
mdarbyy <michaeldarby2698@gmail.com>
2026-08-26 15:29:06 -0400

    Added alert messaging
    added alert messages to appear when there are issues using the app

--- app/controllers/stock_numbers_controller.rb
+++ app/controllers/stock_numbers_controller.rb
@@ -4,9 +4,11 @@ class StockNumbersController < ApplicationController
   end
 
   def index
-    @PER_PAGE = 10
+    @PER_PAGE = 20
     @stock_numbers = Rails.cache.read("stock_numbers")
+    
     return unless @stock_numbers.present?
+
     @page = params[:page].to_i
     @page = 1 if @page < 1
     @total_pages = (@stock_numbers.length.to_f / @PER_PAGE).ceil
@@ -17,17 +19,20 @@ class StockNumbersController < ApplicationController
 
   def upload
     if params[:file].blank?
-      redirect_to root_path, alert: "Please select an Excel file."
+      redirect_to root_path, danger: "Please select an Excel file"
       return
     end
+
     @stock_numbers = StockNumberImporter.new(params[:file]).call
+
     Rails.cache.write(
       "stock_numbers",
       @stock_numbers
     )
+
     redirect_to stock_numbers_path
   rescue StandardError => e
-    redirect_to root_path, alert: e.message
+    redirect_to root_path, danger: e.message
   end
 
 end
